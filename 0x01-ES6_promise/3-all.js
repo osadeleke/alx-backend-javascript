b183390efbc6c0c7e80e3ca22e4dcdc9f79be2e2
Olusegun Adeleke <oadeleke123@gmail.com>
2024-04-10 12:37:50 +0100

    fix: add return

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([response1, response2]) => {
       console.log(`${response1.body} ${response2.firstName} ${response2.lastName}`);
     })
