1e7b51d7460c74f334848fd9fceb9b21f63bdb2a
Olusegun Adeleke <oadeleke123@gmail.com>
2024-04-10 20:18:11 +0100

    fix: handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,21 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((responses) => responses);
+    .then((responses) => {
+      const respArray = [];
+      for (const response of responses) {
+        if (response.status === 'fulfilled') {
+          respArray.push({
+            status: response.status,
+            value: response.value,
+          });
+        } else {
+          respArray.push({
+            status: response.status,
+            value: `${response.reason.name}: ${response.reason.message}`,
+          });
+        }
+      }
+      return respArray;
+    });
 }
