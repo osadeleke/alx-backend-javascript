fdab22c665644568417af0bf32f8129bda2d4368
Olusegun Adeleke <oadeleke123@gmail.com>
2024-04-15 22:00:37 +0100

    fix: not directly changing value of object

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,15 @@
-export default function updateStudentGradeByCity (students, city, newGrade) {
+export default function updateStudentGradeByCity(students, city, newGrade) {
   return students
-  .filter((student) => student.location === city)
-  .map((student) => {
-      student.grade = 'N/A'
+    .filter((student) => student.location === city)
+    .map((student) => {
+      let grade = 'N/A';
       for (const studgrade of newGrade) {
         if (studgrade.studentId === student.id) {
           if (studgrade.grade) {
-            student.grade = studgrade.grade
+            grade = studgrade.grade;
+          }
         }
       }
-    }
-    return student
-  })
+      return { ...student, grade };
+    });
 }
