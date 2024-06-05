b87af6feba112185a15f48461c8d24983da3a97e
Olusegun Adeleke <oadeleke123@gmail.com>
2024-06-05 14:40:56 +0100

    Add: standard in

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  const trimmedData = data.trim();
+  console.log(`Your name is: ${trimmedData}`);
+
+  process.stdin.on('end', () => {
+    console.log('This important software is now closing');
+  });
+});
