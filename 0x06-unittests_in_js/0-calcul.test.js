39ca744f945711329269ebb8b76246427d4fc373
Olusegun Adeleke <oadeleke123@gmail.com>
2024-06-07 10:19:22 +0100

    Fix: test for second rounded number

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,24 +1,24 @@
-const calculateNumber = require("./0-calcul.js");
 const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
-describe('result', function () {
-    it('should return the sum of rounded value of each', function () {
-        const result = calculateNumber(1, 3);
-        assert.equal(result, 4);
-    });
+describe('result', () => {
+  it('should return the sum of rounded value of each', () => {
+    const result = calculateNumber(1, 3);
+    assert.equal(result, 4);
+  });
 
-    it('should return the sum of rounded value of each', function () {
-        const result = calculateNumber(1, 3.7);
-        assert.equal(result, 5);
-    });
+  it('should return the sum of rounded value of each', () => {
+    const result = calculateNumber(1, 3.7);
+    assert.equal(result, 5);
+  });
 
-    it('should return the sum of rounded value of each', function () {
-        const result = calculateNumber(1.2, 3.7);
-        assert.equal(result, 5);
-    });
+  it('should return the sum of rounded value of each', () => {
+    const result = calculateNumber(1.2, 4);
+    assert.equal(result, 5);
+  });
 
-    it('should return the sum of rounded value of each', function () {
-        const result = calculateNumber(1.5, 3.7);
-        assert.equal(result, 6);
-    });
+  it('should return the sum of rounded value of each', () => {
+    const result = calculateNumber(1.5, 3.7);
+    assert.equal(result, 6);
+  });
 });
