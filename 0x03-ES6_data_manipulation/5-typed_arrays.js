bc7dde29fce6e0942f3b222c8861511bd3427501
Olusegun Adeleke <oadeleke123@gmail.com>
2024-06-16 13:47:46 +0100

    Fix: throw error

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,7 +4,7 @@ export default function createInt8TypedArray(length, position, value) {
   if (position >= 0 && position < length) {
     dataView.setInt8(position, value);
   } else {
-    console.error('Position outside range');
+    throw new Error('Position outside range');
   }
 
   return dataView;
