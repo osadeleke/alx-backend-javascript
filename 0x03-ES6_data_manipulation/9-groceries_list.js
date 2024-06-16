9a7f361bdb8b2174eb24cbbfd1e43dd91adaefc8
Olusegun Adeleke <oadeleke123@gmail.com>
2024-06-16 16:42:43 +0100

    Add: map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const aMap = new Map();
+  aMap.set('Apples', 10);
+  aMap.set('Tomatoes', 10);
+  aMap.set('Pasta', 1);
+  aMap.set('Rice', 1);
+  aMap.set('Banana', 5);
+
+  return aMap;
+}
