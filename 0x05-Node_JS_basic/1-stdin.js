6660d1744c417c0994859ab495cf13855ac2a976
Olusegun Adeleke <oadeleke123@gmail.com>
2024-07-24 23:23:14 +0100

    revert update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const trimmedData = data.trim();
   console.log(`Your name is: ${trimmedData}`);
-  console.log('Welcome to Holberton School, what is your name?');
+  // console.log('Welcome to Holberton School, what is your name?');
 
   process.stdin.on('end', () => {
     console.log('This important software is now closing');
